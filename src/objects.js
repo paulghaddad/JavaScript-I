cead680a4fbf3044645eaa3671020da2bc84600b
Paul Haddad <paulh16@gmail.com>
2018-02-23 06:55:44 -0500

    Complete closure exercise

--- src/objects.js
+++ src/objects.js
@@ -43,7 +43,6 @@ const pairs = (obj) => {
 };
 
 /* STRETCH PROBLEMS */
-/* eslint-disable */
 const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
